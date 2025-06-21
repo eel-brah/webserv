b2d5ba09df4db4b47ab01c979bfa8d7e622e8116
El Mehdi El Brahmi <eel-brah@e1r12p4.1337.ma>
2025-06-21 16:49:23 +0100

    Update conf parser

--- ConfigParser.cpp
+++ ConfigParser.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ConfigParser.cpp                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: muel-bak <muel-bak@student.1337.ma>        +#+  +:+       +#+        */
+/*   By: muel-bak <muel-bak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/08 17:17:38 by muel-bak          #+#    #+#             */
-/*   Updated: 2025/06/19 14:13:23 by muel-bak         ###   ########.fr       */
+/*   Updated: 2025/06/21 16:08:22 by muel-bak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,14 +25,6 @@ std::string trim(const std::string& str) {
         --end;
     }
     return str.substr(start, end - start + 1);
-}
-
-std::string remove_comments(const std::string& str) {
-    size_t comment_pos = str.find('#');
-    if (comment_pos != std::string::npos) {
-        return str.substr(0, comment_pos);
-    }
-    return str;
 }
 
 std::vector<std::string> split_with_quotes(const std::string& s) {
@@ -58,41 +50,6 @@ std::vector<std::string> split_with_quotes(const std::string& s) {
         tokens.push_back(token);
     }
     return tokens;
-}
-
-void parse_global_directive(GlobalConfig& global, const std::vector<std::string>& tokens) {
-    if (tokens.empty()) {
-        throw std::runtime_error("Empty global directive");
-    }
-    std::string directive = tokens[0];
-    if (directive == "worker_processes") {
-        if (tokens.size() != 2) throw std::runtime_error("Invalid worker_processes directive");
-        global.worker_processes = std::atoi(tokens[1].c_str());
-    } else if (directive == "pid") {
-        if (tokens.size() != 2) throw std::runtime_error("Invalid pid directive");
-        global.pid = tokens[1];
-    } else if (directive == "include") {
-        if (tokens.size() != 2) throw std::runtime_error("Invalid include directive");
-        global.include = tokens[1];
-    } else if (directive == "default_type") {
-        if (tokens.size() != 2) throw std::runtime_error("Invalid default_type directive");
-        global.default_type = tokens[1];
-    } else {
-        throw std::runtime_error("Unknown global directive: " + directive);
-    }
-}
-
-void parse_events_directive(GlobalConfig& global, const std::vector<std::string>& tokens) {
-    if (tokens.empty()) {
-        throw std::runtime_error("Empty events directive");
-    }
-    std::string directive = tokens[0];
-    if (directive == "worker_connections") {
-        if (tokens.size() != 2) throw std::runtime_error("Invalid worker_connections directive");
-        global.worker_connections = std::atoi(tokens[1].c_str());
-    } else {
-        throw std::runtime_error("Unknown events directive: " + directive);
-    }
 }
 
 void parse_server_directive(ServerConfig& server, const std::vector<std::string>& tokens) {
@@ -125,13 +82,33 @@ void parse_server_directive(ServerConfig& server, const std::vector<std::string>
         for (size_t i = 1; i < tokens.size(); ++i) {
             server.index.push_back(tokens[i]);
         }
-    } else if (directive == "client_max_body_size") {
-        if (tokens.size() != 2) throw std::runtime_error("Invalid client_max_body_size directive");
-        server.client_max_body_size = std::atoi(tokens[1].c_str());
     } else if (directive == "error_page") {
         if (tokens.size() < 3) throw std::runtime_error("Invalid error_page directive");
-        int code = std::atoi(tokens[1].c_str());
-        server.error_pages[code] = tokens[2];
+        for (size_t i = 1; i < tokens.size() - 1; ++i) {
+            int code = std::atoi(tokens[i].c_str());
+            if (code < 100 || code > 599) throw std::runtime_error("Invalid error code: " + tokens[i]);
+            server.error_pages[code] = tokens[tokens.size() - 1];
+        }
+    } else if (directive == "client_max_body_size") {
+        if (tokens.size() != 2) throw std::runtime_error("Invalid client_max_body_size directive");
+        std::string size_str = tokens[1];
+        size_t multiplier = 1;
+        if (!size_str.empty()) {
+            char last_char = size_str[size_str.length() - 1];
+            if (last_char == 'm' || last_char == 'M') {
+                multiplier = 1024 * 1024;
+                size_str = size_str.substr(0, size_str.length() - 1);
+            } else if (last_char == 'k' || last_char == 'K') {
+                multiplier = 1024;
+                size_str = size_str.substr(0, size_str.length() - 1);
+            }
+        }
+        server.client_max_body_size = std::atoi(size_str.c_str()) * multiplier;
+    } else if (directive == "autoindex") {
+        if (tokens.size() != 2 || (tokens[1] != "on" && tokens[1] != "off")) {
+            throw std::runtime_error("Invalid autoindex directive");
+        }
+        server.autoindex = (tokens[1] == "on");
     } else {
         throw std::runtime_error("Unknown server directive: " + directive);
     }
@@ -158,12 +135,35 @@ void parse_location_directive(LocationConfig& location, const std::vector<std::s
         for (size_t i = 1; i < tokens.size(); ++i) {
             location.try_files.push_back(tokens[i]);
         }
-    } else if (directive == "redirect") {
-        if (tokens.size() != 2) throw std::runtime_error("Invalid redirect directive");
-        location.redirect = tokens[1];
-    } else if (directive == "autoindex") {
-        if (tokens.size() != 2) throw std::runtime_error("Invalid autoindex directive");
-        location.autoindex = (tokens[1] == "on");
+    } else if (directive == "proxy_pass") {
+        if (tokens.size() != 2) throw std::runtime_error("Invalid proxy_pass directive");
+        location.proxy_pass = tokens[1];
+    } else if (directive == "proxy_set_header") {
+        if (tokens.size() != 3) throw std::runtime_error("Invalid proxy_set_header directive");
+        location.proxy_set_headers[tokens[1]] = tokens[2];
+    } else if (directive == "expires") {
+        if (tokens.size() != 2) throw std::runtime_error("Invalid expires directive");
+        location.expires = tokens[1];
+    } else if (directive == "access_log") {
+        if (tokens.size() != 2) throw std::runtime_error("Invalid access_log directive");
+        location.access_log = tokens[1];
+    } else if (directive == "auth_basic") {
+        if (tokens.size() < 2) throw std::runtime_error("Invalid auth_basic directive");
+        std::string value;
+        for (size_t i = 1; i < tokens.size(); ++i) {
+            value += tokens[i];
+            if (i < tokens.size() - 1) value += " ";
+        }
+        if (!value.empty() && value[0] == '"' && value[value.length() - 1] == '"') {
+            value = value.substr(1, value.length() - 2);
+        }
+        location.auth_basic = value;
+    } else if (directive == "auth_basic_user_file") {
+        if (tokens.size() != 2) throw std::runtime_error("Invalid auth_basic_user_file directive");
+        location.auth_basic_user_file = tokens[1];
+    } else if (directive == "deny") {
+        if (tokens.size() != 2) throw std::runtime_error("Invalid deny directive");
+        location.deny = tokens[1];
     } else if (directive == "index") {
         if (tokens.size() < 2) throw std::runtime_error("Invalid index directive");
         location.index.clear();
@@ -176,9 +176,21 @@ void parse_location_directive(LocationConfig& location, const std::vector<std::s
     } else if (directive == "cgi_bin") {
         if (tokens.size() != 2) throw std::runtime_error("Invalid cgi_bin directive");
         location.cgi_bin = tokens[1];
-    } else if (directive == "upload_path") {
-        if (tokens.size() != 2) throw std::runtime_error("Invalid upload_path directive");
-        location.upload_path = tokens[1];
+    } else if (directive == "return") {
+        if (tokens.size() != 3) throw std::runtime_error("Invalid return directive");
+        location.redirect_code = std::atoi(tokens[1].c_str());
+        if (location.redirect_code < 300 || location.redirect_code > 399) {
+            throw std::runtime_error("Invalid redirect code: " + tokens[1]);
+        }
+        location.redirect_url = tokens[2];
+    } else if (directive == "autoindex") {
+        if (tokens.size() != 2 || (tokens[1] != "on" && tokens[1] != "off")) {
+            throw std::runtime_error("Invalid autoindex directive");
+        }
+        location.autoindex = (tokens[1] == "on");
+    } else if (directive == "upload_store") {
+        if (tokens.size() != 2) throw std::runtime_error("Invalid upload_store directive");
+        location.upload_store = tokens[1];
     } else {
         throw std::runtime_error("Unknown location directive: " + directive);
     }
@@ -191,38 +203,26 @@ std::vector<ServerConfig> parseConfig(const std::string& file) {
     }
 
     std::vector<ServerConfig> configs;
-    GlobalConfig global = {0, "", 0, "", ""};
     std::string line;
     ServerConfig current_server;
     LocationConfig current_location;
     std::vector<LocationConfig*> location_stack;
-    bool in_events = false, in_http = false, in_server = false, in_location = false;
+    bool in_server = false, in_location = false;
 
     while (std::getline(ifs, line)) {
-        line = remove_comments(line);
         line = trim(line);
-        if (line.empty()) continue;
+        if (line.empty() || line[0] == '#') continue;
 
         if (!line.empty() && line[line.length() - 1] == '{') {
             std::vector<std::string> tokens = split_with_quotes(line.substr(0, line.length() - 1));
-            if (tokens.size() == 1 && tokens[0] == "events") {
-                if (in_events || in_http || in_server || in_location) {
-                    throw std::runtime_error("Misplaced events block");
-                }
-                in_events = true;
-            } else if (tokens.size() == 1 && tokens[0] == "http") {
-                if (in_events || in_http || in_server || in_location) {
-                    throw std::runtime_error("Misplaced http block");
-                }
-                in_http = true;
-            } else if (tokens.size() == 1 && tokens[0] == "server") {
-                if (!in_http) throw std::runtime_error("Server block outside http block");
+            if (tokens.size() == 1 && tokens[0] == "server") {
                 if (in_server) throw std::runtime_error("Nested server blocks not allowed");
                 in_server = true;
                 current_server = ServerConfig();
                 current_server.host = "0.0.0.0";
-                current_server.port = 80;
-                current_server.client_max_body_size = 1048576; // Default 1MB
+                current_server.port = 0;
+                current_server.client_max_body_size = 0; // Default
+                current_server.autoindex = true;
             } else if (tokens[0] == "location") {
                 if (!in_server && !in_location) throw std::runtime_error("Location outside of server or location block");
                 LocationConfig new_location;
@@ -230,9 +230,13 @@ std::vector<ServerConfig> parseConfig(const std::string& file) {
                     new_location.path = tokens[1];
                 } else if (tokens.size() == 3 && tokens[1] == "=") {
                     new_location.path = "= " + tokens[2];
+                } else if (tokens.size() == 3 && tokens[1] == "^~") {
+                    new_location.path = "^~ " + tokens[2];
                 } else {
-                    throw std::runtime_error("Invalid location directive: " + line);
+                    throw std::runtime_error("Invalid location directive (regex not allowed): " + line);
                 }
+                new_location.redirect_code = 0; // Default
+                new_location.autoindex = false; // Default
                 if (in_location) {
                     location_stack.back()->nested_locations.push_back(new_location);
                     location_stack.push_back(&location_stack.back()->nested_locations.back());
@@ -251,10 +255,6 @@ std::vector<ServerConfig> parseConfig(const std::string& file) {
             } else if (in_server) {
                 configs.push_back(current_server);
                 in_server = false;
-            } else if (in_http) {
-                in_http = false;
-            } else if (in_events) {
-                in_events = false;
             } else {
                 throw std::runtime_error("Unmatched closing brace");
             }
@@ -268,27 +268,21 @@ std::vector<ServerConfig> parseConfig(const std::string& file) {
                 parse_location_directive(*location_stack.back(), tokens);
             } else if (in_server) {
                 parse_server_directive(current_server, tokens);
-            } else if (in_events) {
-                parse_events_directive(global, tokens);
-            } else if (in_http) {
-                parse_global_directive(global, tokens);
             } else {
-                parse_global_directive(global, tokens);
+                throw std::runtime_error("Directives must be inside server block");
             }
         } else {
             throw std::runtime_error("Invalid line: " + line);
         }
     }
 
-    if (in_events || in_http || in_server || in_location) {
+    if (in_server || in_location) {
         throw std::runtime_error("Unclosed block in config file");
     }
 
     ifs.close();
     return configs;
 }
-
-// Path matching functions
 
 std::string toLower(const std::string& str) {
     std::string result = str;
@@ -307,16 +301,6 @@ bool endsWith(const std::string& str, const std::string& suffix) {
     return str.substr(str.length() - suffix.length()) == suffix;
 }
 
-bool endsWithAnyCaseInsensitive(const std::string& str, const std::string& suffixes) {
-    std::string lowerStr = toLower(str);
-    std::string lowerSuffixes = toLower(suffixes);
-    if (lowerSuffixes[0] != '.' || lowerSuffixes[lowerSuffixes.length() - 1] != '$') {
-        return false;
-    }
-    std::string ext = lowerSuffixes.substr(1, lowerSuffixes.length() - 2);
-    return endsWith(lowerStr, "." + ext);
-}
-
 bool isPathCompatible(const std::string& locationPath, const std::string& requestedPath) {
     if (locationPath.empty() || requestedPath.empty()) {
         return false;
@@ -333,8 +317,74 @@ bool isPathCompatible(const std::string& locationPath, const std::string& reques
         return reqPath == exactPath;
     }
 
+    if (locPath.length() >= 3 && locPath.substr(0, 3) == "^~ ") {
+        std::string prefix = locPath.substr(3);
+        if (reqPath.length() >= prefix.length()) {
+            return reqPath.substr(0, prefix.length()) == prefix;
+        }
+        return false;
+    }
+
     if (reqPath.length() >= locPath.length()) {
         return reqPath.substr(0, locPath.length()) == locPath;
     }
     return false;
+}
+
+std::vector<ServerConfig> generateDefaultConfig() {
+    std::vector<ServerConfig> configs;
+    ServerConfig default_server;
+
+    // Default server settings
+    default_server.host = "0.0.0.0";
+    default_server.port = 80;
+    default_server.server_names.push_back("localhost");
+    default_server.client_max_body_size = 1048576; // 1MB
+    default_server.root = "/var/www/html";
+    default_server.index.push_back("index.html");
+    default_server.index.push_back("index.htm");
+    default_server.autoindex = false;
+    default_server.error_pages[404] = "/404.html";
+    default_server.error_pages[500] = "/404.html";
+
+    // Default location for "/"
+    LocationConfig default_location;
+    default_location.path = "/";
+    default_location.allowed_methods.push_back("GET");
+    default_location.allowed_methods.push_back("POST");
+    default_location.root = "/var/www/html";
+    default_location.index.push_back("index.html");
+    default_location.index.push_back("index.htm");
+    default_location.autoindex = false;
+    default_location.redirect_code = 0; // No redirect
+
+    // Default location for "/upload/"
+    LocationConfig upload_location;
+    upload_location.path = "/upload/";
+    upload_location.allowed_methods.push_back("POST");
+    upload_location.upload_store = "/tmp/uploads";
+    upload_location.root = "/var/www/html";
+    upload_location.autoindex = false;
+    upload_location.redirect_code = 0;
+
+    // Default location for "/php/"
+    LocationConfig php_location;
+    php_location.path = "/php/";
+    php_location.allowed_methods.push_back("GET");
+    php_location.allowed_methods.push_back("POST");
+    php_location.root = "/var/www/php";
+    php_location.cgi_ext = ".php";
+    php_location.cgi_bin = "/usr/bin/php-cgi";
+    php_location.autoindex = false;
+    php_location.redirect_code = 0;
+
+    // Add locations to server
+    default_server.locations.push_back(default_location);
+    default_server.locations.push_back(upload_location);
+    default_server.locations.push_back(php_location);
+
+    // Add server to configs
+    configs.push_back(default_server);
+
+    return configs;
 }
--- ConfigParser.hpp
+++ ConfigParser.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ConfigParser.hpp                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: muel-bak <muel-bak@student.1337.ma>        +#+  +:+       +#+        */
+/*   By: muel-bak <muel-bak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/06/08 17:17:38 by muel-bak          #+#    #+#             */
-/*   Updated: 2025/06/19 14:13:30 by muel-bak         ###   ########.fr       */
+/*   Created: 2025/06/14 18:33:49 by muel-bak          #+#    #+#             */
+/*   Updated: 2025/06/21 16:09:12 by muel-bak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,28 +22,27 @@
 #include <cstdlib>
 #include <stdexcept>
 
-// Global configuration struct to store top-level directives
-struct GlobalConfig {
-    int worker_processes;
-    std::string pid;
-    int worker_connections; // From events block
-    std::string include; // e.g., mime.types
-    std::string default_type;
-};
-
 struct LocationConfig {
-    std::string path; // e.g., "/api"
-    std::vector<std::string> allowed_methods; // e.g., {"GET", "POST"}
+    std::string path; // e.g., "/api", "= /"
+    std::vector<std::string> allowed_methods; // e.g., {"POST", "GET"}
     std::string root; // e.g., "/var/www/api"
     std::string alias; // e.g., "/var/www/static"
     std::vector<std::string> try_files; // e.g., {"$uri", "$uri/", "/index.html"}
-    std::string redirect; // e.g., "http://example.com/newpath"
-    bool autoindex; // Directory listing: true = on, false = off
-    std::vector<std::string> index; // e.g., {"index.html", "index.htm"}
+    std::string proxy_pass; // e.g., "http://backend:8080"
+    std::map<std::string, std::string> proxy_set_headers; // e.g., {"Host", "$host"}
+    std::string expires; // e.g., "1y"
+    std::string access_log; // e.g., "off"
+    std::string auth_basic; // e.g., "Restricted Area"
+    std::string auth_basic_user_file; // e.g., "/etc/nginx/.htpasswd"
+    std::string deny; // e.g., "all"
     std::string cgi_ext; // e.g., ".php"
     std::string cgi_bin; // e.g., "/usr/bin/php-cgi"
-    std::string upload_path; // e.g., "/var/www/uploads"
     std::vector<LocationConfig> nested_locations; // For nested location blocks
+    std::vector<std::string> index; // e.g., {"index.html", "index.htm"}
+    int redirect_code; // e.g., 301
+    std::string redirect_url; // e.g., "/newpath"
+    bool autoindex; // e.g., true for "on"
+    std::string upload_store; // e.g., "/tmp/uploads"
 };
 
 struct ServerConfig {
@@ -55,12 +54,11 @@ struct ServerConfig {
     std::string root; // e.g., "/var/www/html"
     std::vector<std::string> index; // e.g., {"index.html", "index.htm"}
     std::vector<LocationConfig> locations;
+    bool autoindex; // e.g., true for "on"
 };
-
-
 
 std::vector<ServerConfig> parseConfig(const std::string& file);
 bool isPathCompatible(const std::string& locationPath, const std::string& requestedPath);
-bool endsWithAnyCaseInsensitive(const std::string& str, const std::string& suffixes);
+std::vector<ServerConfig> generateDefaultConfig();
 
 #endif // CONFIG_PARSER_HPP
