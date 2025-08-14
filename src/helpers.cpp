d1b9abed57be8668cc8e7bbcf9f25b100e2b1e2d
El Mehdi <mehdi.elbrahmi@proton.me>
2025-08-14 16:40:00 +0100

    UPdate

--- src/helpers.cpp
+++ src/helpers.cpp
@@ -200,7 +200,7 @@ std::string random_string() {
   int rand_number = rand();
 
   std::stringstream filename;
-  filename << "file_" << rand_number << "_" << std::time(0);
+  filename << rand_number << "_" << std::time(0);
   return filename.str();
 }
 
